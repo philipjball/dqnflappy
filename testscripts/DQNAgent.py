6ed8cc57577b8b41f5ce25a54e2f4d481c1e9a35
Philip Ball <t-phball@microsoft.com>
2018-11-08 00:34:55 +0000

    Committing some updates to the DQN network to calculate sizes

--- testscripts/DQNAgent.py
+++ testscripts/DQNAgent.py
@@ -17,9 +17,11 @@ class ReplayMemory(object):
 
     def push(self, *args):
         """Saves a transition."""
+        # Stop appending when we reach capacity
         if len(self.memory) < self.capacity:
             self.memory.append(None)
         self.memory[self.position] = Transition(*args)
+        # Modulo to wrap around when we reach the end of the list
         self.position = (self.position + 1) % self.capacity
 
     def sample(self, batch_size):
@@ -35,7 +37,7 @@ class DQN(nn.Module):
 		self.conv1 = nn.Conv2d(3, 16, kernel_size=5, stride=2)
 		self.conv2 = nn.Conv2d(16, 32, kernel_size=5, stride=2)
 		self.conv3 = nn.Container(32, 32, kernel_size=5, stride=2)
-		self.linear1 = nn.Linear(, 64)
+		self.linear1 = nn.Linear(self.calculate_final_size(input_height, input_width) , 64)
 		self.linear2 = nn.Linear(64, num_actions)
 	
 	@staticmethod
@@ -48,7 +50,10 @@ class DQN(nn.Module):
 		return int(h_out), int(w_out)
 
 	def calculate_final_size(input_height, input_width):
-		
+		ho1, wo1 = self.calculate_conv_out(input_height, input_width, 5, 2)
+		ho2, wo2 = self.calculate_conv_out(ho1, wo1, 5, 2)
+		ho3, wo3 = self.calculate_conv_out(ho2, wo2, 5, 2)
+		return int(ho3 * wo3 * 32)
 
 
 	def forward(x):
@@ -57,6 +62,8 @@ class DQN(nn.Module):
 		x = self.conv3(x)
 		return self.linear2(linear1(x.view(x.size(0), -1)))
 
+class Trainer(object):
 
+	def __init__(self, )
     
 
